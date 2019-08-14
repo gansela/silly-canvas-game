75467cd41bb87566324b82b179e2b763e5453765
gansela <gan.sela@gmail.com>
2019-08-14 22:31:00 +0300

    first commit, wanky phissics

---
+++ game.js
@@ -0,0 +1,119 @@
+
+const brush = canvas.getContext("2d")
+
+const player = new Kikele(300, 300, 0, 0, 20)
+
+const enemies = [
+    new Kikele(50, 100, 0, 0, 20),
+    new Kikele(1300, 500, 0, 0, 20),
+    new Kikele(1100, 50, 0, 0, 20),
+    new Kikele(50, 500, 0, 0, 20),
+    new Kikele(800, 400, 0, 0, 20)
+]
+
+let stopAnimate = false
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+const weapon = {
+    x: undefined,
+    y: undefined,
+    counter: 0
+}
+
+let counter = 0
+
+function animate() {
+    if (stopAnimate) return
+    brush.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    requestAnimationFrame(animate)
+    player.drawPlayer("navy")
+    enemies.forEach(enemy => enemy.drawPlayer("darkgreen"))
+    player.move()
+    enemies.forEach(function (enemy, i) {
+        if (Math.abs(player.x - enemy.x) < 20 && Math.abs(player.y - enemy.y) < 20) {
+            alert("you lose")
+            stopAnimate = true
+        }
+        setTimeout(function () {
+            enemies.forEach(enemy => enemy.attack())
+        }, 1000);
+    })
+    shoot(player)
+}
+
+
+document.onkeydown = function (e) {
+    if (e.keyCode === 13) {
+        animate()
+
+    }
+}
+
+canvas.onkeydown = function (e) {
+    if (e.keyCode === 27) {
+        console.log("esc")
+        stopAnimate = true
+
+    }
+}
+
+
+function frictionX(circle) {
+    if (circle.dx === 0) return
+    else if (circle.dx < 0) circle.dx += (-circle.dx / 100)
+    else circle.dx -= (circle.dx / 100)
+}
+
+function frictionY(circle) {
+    if (circle.dy === 0) return
+    else if (circle.dy < 0) circle.dy += (-circle.dy / 100)
+    else circle.dy -= (circle.dy / 100)
+}
+
+function limit(val) {
+    if (val > 5) val = 5;
+    if (val < -5) val = -5
+}
+
+function shoot(player) {
+    canvas.onclick = function (e) {
+        mouse.x = e.x
+        mouse.y = e.y
+        weapon.x = player.x
+        weapon.y = player.y
+        weapon.counter = 0
+        enemies.forEach(function (enemy, i) {
+            if (Math.abs(mouse.x - enemy.x) < 18 && Math.abs(mouse.y - enemy.y) < 18) {
+                enemies.splice(i, 1)
+            }
+        })
+    }
+    line()
+    weapon.counter++
+}
+
+function line() {
+    if (weapon.counter < 10) {
+        let lineX = 0;
+        let lineY = 0
+        if (weapon.counter > 0) {
+            if (mouse.x > weapon.x) weapon.x += (mouse.x - weapon.x) / 5
+            else weapon.x -= (weapon.x - mouse.x) / 5
+            if (mouse.y > weapon.y) weapon.y += (mouse.y - weapon.y) / 5
+            else weapon.y -= (weapon.y - mouse.y) / 5
+        }
+        brush.beginPath()
+        brush.moveTo(weapon.x, weapon.y);
+        brush.lineTo(mouse.x, mouse.y)
+        brush.lineWidth = 3;
+        brush.strokeStyle = "red"
+        brush.stroke()
+    }
+}
+
+// const playerBounderies = player.getBoundingClientRect();
+// console.log(playerBounderies)
---
+++ models.js
@@ -0,0 +1,83 @@
+class Kikele {
+
+    constructor(x, y, dx, dy, radius, ) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius
+    }
+
+    drawPlayer( color) {
+        brush.beginPath();
+        brush.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        brush.fillStyle = color
+        brush.fill();
+        this.y = this.y + this.dy;
+        this.x = this.x + this.dx;
+        if (this.x - this.radius < 0 || this.x + this.radius > innerWidth) {
+            this.dx = -this.dx
+        }
+        if (this.y - this.radius < 0 || this.y + this.radius > innerHeight) {
+            this.dy = -this.dy
+        }
+        limit(this.dx)
+        limit(this.dy)
+        // frictionX(this)
+        // frictionY(this)
+    }
+    move() {
+        document.onkeydown = function (e) {
+            switch (e.keyCode) {
+                case 65:
+                    player.moveLeft();
+                    // draw()
+                    break;
+                case 87:
+                    player.moveUp();
+                    // draw()
+                    break;
+                case 68:
+                    player.moveRight();
+                    // draw()
+                    break;
+                case 83:
+                    player.moveDown();
+                    // draw()
+                    break;
+                case 27:
+                    stopAnimate = true
+                    break;
+            }
+        };
+    }
+
+    moveUp() {
+        this.dy -= 1
+    }
+
+    moveDown() {
+        this.dy += 1
+    }
+
+    moveRight() {
+        this.dx += 1
+    }
+
+    moveLeft() {
+        this.dx -= 1
+    }
+    attack(){
+        if(this.x < player.x) this.dx++
+        else this.dx--
+        if(this.y < player.y) this.dy++
+        else this.dy--
+        if (Math.abs(this.dx) > 5 || Math.abs(this.dy) > 5) {
+            this.dx = 0
+            this.dy = 0
+        }
+    }
+
+}
+
+
